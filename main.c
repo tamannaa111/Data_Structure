85cefd94d07b8907cefa32877df926c1d585bc04
tamannaa111 <144879732+tamannaa111@users.noreply.github.com>
2023-09-13 06:19:42 -0700

    Add files via upload
    InsertEnd

--- main.c
+++ main.c
@@ -1,80 +1,49 @@
-#include<stdio.h>
+#include <stdio.h>
 #include<stdlib.h>
 #include<string.h>
 
-
 struct node{
-    
-    char data[10];
+    int data;
     struct node *next;
     
 };
-typedef struct node Node;
-Node *head=NULL;
+
+struct node *head=NULL;
 
 void print()
 {
     printf("printing the linked list:\n");
-    
-    Node *p=head;
+   struct node *p=head;
     while (p!=NULL) {
-        printf("%s ",p->data);
+        printf("%d ",p->data);
         p=p->next;
     }printf("\n");
-    
-    
 }
-
-void insertFirst(){
-char data[10];
-    printf("*****Inserting node before head*****\n");
-    Node *newNode;
-    newNode=(Node*)malloc(sizeof(Node));
-    
-    printf("Enter data: ");
-    scanf("%s",data);
-    
-    strcpy(newNode->data,data);
-    newNode->next=head;
-    head=newNode;
-    
-    
-}
-
-void insertEnd(){
-    
-    char data[10];
-    printf("****8Inserting data at the End*****\n");
-    Node *newNode;
-    newNode=(Node*)malloc(sizeof(Node));
-    
-    printf("Enter data item: ");
-    scanf("%s",data);
-    
-   strcpy( newNode->data,data);
+void insertEnd()
+{
+    printf("Inserting Node before head:\n");
+    int data;
+    struct node *newNode;
+    newNode =(struct node*)malloc(sizeof(struct node));
+    printf("enter the data item:\n");
+    scanf("%d",&data);
+    newNode->data=data;
     newNode->next=NULL;
-    
-    Node *ptr = head;
+    struct node *ptr = head ;
     while (ptr->next!=NULL) {
-        ptr=ptr->next;     //ptr++;
+        ptr=ptr->next;
     }
     ptr->next=newNode;
     
-    
+ 
 }
-int main(){
-    insertFirst();
-    print();
-    insertFirst();
-    print();
-    insertEnd();
-    print();
-    
-    
-    insertEnd();
-    print();
-    
-   
-    
-}
-
+int main()
+                     {
+        insertEnd();
+       
+        print();
+         insertEnd();
+      print();
+        return 0;
+                     }
+                      
